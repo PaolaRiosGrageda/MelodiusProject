cc82d49f2942be1f91ea980c9caf74799fce5510
Emmanuel Mercado <Emmanuel.Mercado@jala.university>
2023-04-12 15:58:55 -0400

    chore: Add basic request for Album and Playlist

--- MelodiusProject/Controllers/AlbumController.cs
+++ MelodiusProject/Controllers/AlbumController.cs
@@ -1,11 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MelodiusDataTransfer;
+using MelodiusServices.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MelodiusAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AlbumController
+    public class AlbumController : ControllerBase
     {
-        
+        private readonly IAlbumService _albumsService;
+
+        public AlbumController(IAlbumService albumService)
+        {
+            _albumsService = albumService;
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult<List<AlbumDto>>> GetAlbums()
+        {
+            try
+            {
+                var albums = await _albumsService.GetAll();
+                return Ok(albums);
+            }
+            catch (Exception e)
+            {
+                var badResponse = new { error = e.Message };
+                return BadRequest(badResponse);
+            }
+
+        }
+
+        [HttpPost()]
+        public async Task<ActionResult> AddNewAlbum([FromBody] AlbumDto album)
+        {
+            try
+            {
+                var newId = await _albumsService.AddNew(album);
+                var anonymousResponse = new
+                {
+                    newId
+                };
+                return Ok(anonymousResponse);
+            }
+            catch (Exception e)
+            {
+                var badResponse = new { error = e.Message };
+                return BadRequest(badResponse);
+            }
+        }
+
+        [HttpPut()]
+        public async Task<ActionResult> UpdateAlbum([FromBody] AlbumDto album)
+        {
+            try
+            {
+                var albumUdpated = await _albumsService.Update(album);
+                return Ok(albumUdpated);
+            }
+            catch (Exception e)
+            {
+                var badResponse = new { error = e.Message };
+                return BadRequest(badResponse);
+            }
+        }
     }
 }
--- MelodiusProject/Controllers/PlaylistController.cs
+++ MelodiusProject/Controllers/PlaylistController.cs
@@ -1,11 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MelodiusDataTransfer;
+using MelodiusServices.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MelodiusAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class PlaylistController
+    public class PlaylistController : ControllerBase
     {
+        private readonly IPlaylistService _playlistsService;
 
+        public PlaylistController(IPlaylistService playlistService)
+        {
+            _playlistsService = playlistService;
+        }
+
+
+        [HttpGet()]
+        public async Task<ActionResult<List<PlaylistDto>>> GetPlaylists()
+        {
+            try
+            {
+                var playlists = await _playlistsService.GetAll();
+                return Ok(playlists);
+            }
+            catch (Exception e)
+            {
+                var badResponse = new { error = e.Message };
+                return BadRequest(badResponse);
+            }
+
+        }
+
+        [HttpPost()]
+        public async Task<ActionResult> AddNewPlaylist([FromBody] PlaylistDto playlist)
+        {
+            try
+            {
+                var newId = await _playlistsService.AddNew(playlist);
+                var anonymousResponse = new
+                {
+                    newId
+                };
+                return Ok(anonymousResponse);
+            }
+            catch (Exception e)
+            {
+                var badResponse = new { error = e.Message };
+                return BadRequest(badResponse);
+            }
+        }
+
+        [HttpPut()]
+        public async Task<ActionResult> UpdatePlaylist([FromBody] PlaylistDto playlist)
+        {
+            try
+            {
+                var playlistUdpated = await _playlistsService.Update(playlist);
+                return Ok(playlistUdpated);
+            }
+            catch (Exception e)
+            {
+                var badResponse = new { error = e.Message };
+                return BadRequest(badResponse);
+            }
+        }
     }
 }
